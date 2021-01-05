bac306326bc8dfbbcf289be5518924177a738e90
@Jean-Franck <jeanfranck.tang@gmail.com>
2021-01-05 12:28:57 +0100

    début

---
+++ client/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <form @submit.prevent="login">
+            <h2>Connexion</h2>
+            <input type="text" v-model="user.email" placeholder="Email" required>
+            <input type="password" v-model="user.password" placeholder="Mot de passe" required>
+            <button type="submit">Se connecter</button>
+        </form>
+        <button><a href="./#/register">Pas encore inscrit ?</a></button>
+    </div>
+</template>
+ 
+<script>
+module.exports = {
+  props: {
+    articles: { type: Array, adefault: [] },
+    panier: { type: Object },
+  },
+  data () {
+    return {
+      user: {
+        email: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    login () {
+      this.$emit('login', this.user)
+    },
+  }
+}
+</script>
+ 
+<style>
+</style>
---
+++ client/components/Register.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <form @submit.prevent="addUser">
+            <h2>Création de compte</h2>
+            <input type="text" v-model="user.email" placeholder="Email" required>
+            <input type="password" v-model="user.password" placeholder="Mot de passe" required>
+            <button type="submit">S'enregistrer</button>
+        </form>
+    </div>
+</template>
+ 
+<script>
+module.exports = {
+  data () {
+    return {
+      user: {
+        email: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    addUser () {
+      this.$emit('add-user', this.user)
+    },
+  }
+}
+</script>
+ 
+<style>
+</style>
