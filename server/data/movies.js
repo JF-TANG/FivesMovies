aafa8321f9740b1dac95b72db7045c73c0605bda
@Jean-Franck <jeanfranck.tang@gmail.com>
2021-01-05 15:01:14 +0100

    English Database

---
+++ server/data/movies.js
@@ -0,0 +1,19 @@
+const movies = [
+  {
+    idFilm: 1,
+    title: 'Avengers',
+    releaseDate: 2012,
+    plot: "Les interprètes des six Avengers sont Robert Downey Jr., Chris Evans, Chris Hemsworth, Scarlett Johansson, Jeremy Renner et Mark Ruffalo. Dans ce premier des quatre films de la série Avengers, Iron Man, Captain America, Thor, Hulk, Clint Barton et Natasha Romanoff doivent tenter de travailler en équipe afin d'empêcher le frère adoptif de Thor, Loki, d'envahir la Terre.",
+    poster:'https://upload.wikimedia.org/wikipedia/en/8/8a/The_Avengers_%282012_film%29_poster.jpg',
+    idUser:1
+  },
+  {
+    idFilm: 2,
+    title: "Avengers : L'Ère d'Ultron",
+    releaseDate: 2015,
+    plot: "Alors que Tony Stark tente de relancer un programme de maintien de la paix jusque-là suspendu, les choses tournent mal et les super-héros Iron Man, Captain America, Thor, Hulk, Black Widow et Hawkeye vont devoir à nouveau unir leurs forces pour combattre le plus puissant de leurs adversaires : le terrible Ultron, un être technologique terrifiant qui s’est juré d’éradiquer l’espèce humaine. Afin d’empêcher celui-ci d’accomplir ses sombres desseins, des alliances inattendues se scellent, les entraînant dans une incroyable aventure et une haletante course contre le temps…",
+    poster:'https://upload.wikimedia.org/wikipedia/en/f/ff/Avengers_Age_of_Ultron_poster.jpg',
+    idUser:1
+  }
+]
+module.exports = movies
